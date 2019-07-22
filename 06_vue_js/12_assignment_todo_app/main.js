f112c9c765c8d927bf51601ac65a077e2aaa7490
juandsc <juansanchezc89@gmail.com>
2019-07-21 23:13:15 -0500

    advance in assignment

--- 06_vue_js/12_assignment_todo_app/main.js
+++ 06_vue_js/12_assignment_todo_app/main.js
@@ -4,8 +4,12 @@ Vue.component("task-list", {
             type: Array,
             required: true
         },
+        remaining: {
+            type: Number,
+            required: true
+        },
     },
-    data: function () {
+    data () {
         return {
             new_task: null,
             error: null
@@ -20,37 +24,40 @@ Vue.component("task-list", {
             } else {
                 this.error = "Ingrese una nueva tarea!";
             }
-        }
+        },
+        removeTask(keyToRemove) {
+            this.$emit("remove-task", keyToRemove);
+        },
     },
     template: `
-        <div class="mt-2">
-            <div class="container">
-                <h3>{{ error }}</h3>
-                <form @submit.prevent="submitTask" class="mb-3">
-                    <div class="form-group">
-                        <label for="newTask">Añada una nueva tarea</label>
-                        <input class="form-control" id="newTask" type="text"
-                                v-model="new_task">
-                        </input>
-                    </div>
-                    <button class="btn btn-sm btn-primary" type="submit">
-                        Añadir
-                    </button>
-                </form>
-                <hr>
-                <single-task v-for="(task, index) in tasks" :task="task" 
-                        :key="index">
-                </single-task>
+        <div class="container mt-2">
+            <p><strong>Remaining Tasks: {{ remaining }}</strong></p>
+            <input type="text"
+                    class="form-control"
+                    placeholder="Qué necesitas hacer?"
+                    v-model="new_task"
+                    @keyup.enter="submitTask">
+            <br>
+            <div class="single-task" 
+                    v-for="(task, index) in tasks" 
+                    :task="task" 
+                    :key="index">
+                {{ task }}
             </div>
         </div>
     `
 })
 
-Vue.component("single-task", {
+Vue.component("single-tas", {
     props: {
         task: {
             type: String,
             required: true
+        }
+    },
+    methods: {
+        submitRemoveTask() {
+            this.$emit("submit-remove-task", this.key);
         }
     },
     template: `
@@ -60,7 +67,7 @@ Vue.component("single-task", {
                         show w-50" role="alert">
                     <strong>{{ task }}</strong> 
                     <button type="button" class="btn close" data-dismiss="alert" 
-                            aria-label="Close"> 
+                            aria-label="Close" :click="submitRemoveTask"> 
                         <span aria-hidden="true">×</span> 
                     </button> 
                 </div> 
@@ -74,9 +81,39 @@ var app = new Vue({
     data: {
         tasks: [],
     },
+    computed: {
+        taskCount() {
+            return this.tasks.length;
+        }
+    },
     methods: {
         addNewTask(new_task) {
             this.tasks.push(new_task);
-        }
+        },
+        removeTask(task) {
+            this.tasks.splice(this.tasks.indexOf(task), 1);
+        },
     },
 })
+
+// `
+//     <div class="container mt-2">
+//         <p>Tareas sobrantes: {{ remaining }}</p>
+//         <h3>{{ error }}</h3>
+//         <form @submit.prevent="submitTask" class="mb-3">
+//             <div class="form-group">
+//                 <label for="newTask">Añada una nueva tarea</label>
+//                 <input class="form-control" id="newTask" type="text"
+//                         v-model="new_task">
+//                 </input>
+//             </div>
+//             <button class="btn btn-sm btn-primary" type="submit">
+//                 Añadir
+//             </button>
+//         </form>
+//         <hr>
+//         <single-task v-for="(task, index) in tasks" :task="task" 
+//                 :key="index" @submit-remove-task="removeTask">
+//         </single-task>
+//     </div>
+// `
