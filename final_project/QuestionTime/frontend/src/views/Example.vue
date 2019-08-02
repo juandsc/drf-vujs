c739901612f726476e648f85310b0f553fce3a6e
juandsc <juansanchezc89@gmail.com>
2019-08-02 17:47:52 -0500

    advance until vue router

---
+++ final_project/QuestionTime/frontend/src/views/Example.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="example">
+    <h1>Este es un ejemplo de componente!</h1>
+    <h3>{{ message }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "example",
+    data() {
+        return {
+            message: "Hello world!!"
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
